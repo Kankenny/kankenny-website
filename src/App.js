df96f2482c1595c092495a575b797d853ebc1d1d
Kenny <kenji.maddela01@gmail.com>
2022-11-27 18:04:14 -0500

    Establish main routes for the webpage: home, contacts, projects and assign a path to each of them accordingly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import Contact from '../layout/Contact'
+import Home from '../layout/Home'
+import Projects from '../layout/Projects'
 
 function App() {
-	return <div className="text-5xl h-screen bg-red-500">Hello world!</div>
+	return (
+		<Routes className="text-5xl h-screen bg-red-500">
+			Hello World!
+			<Route path="/" element={<Home />}></Route>
+			<Route path="/contact" element={<Contact />}></Route>
+			<Route path="/projects" element={<Projects />}></Route>
+		</Routes>
+	)
 }
 
 export default App
