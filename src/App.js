97935297a7482b8a9effc2a90991e8b62f586717
Kenny <kenji.maddela01@gmail.com>
2022-11-27 18:12:41 -0500

    Relocate layout folder inside of src due to some error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 
-import Contact from '../layout/Contact'
-import Home from '../layout/Home'
-import Projects from '../layout/Projects'
-import Error404 from '../layout/Error404'
+import Contact from './layout/Contact'
+import Home from './layout/Home'
+import Projects from './layout/Projects'
+import Error404 from './layout/Error404'
 
 function App() {
 	return (
-		<Routes className="text-5xl h-screen bg-red-500">
-			Hello World!
-			<Route path="/" element={<Home />}></Route>
-			<Route path="/contact" element={<Contact />}></Route>
-			<Route path="/projects" element={<Projects />}></Route>
-			<Route path="*" element={<Error404 />}></Route>
-		</Routes>
+		<div className="h-screen bg-red-500">
+			<Routes>
+				Hello World!
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/contact" element={<Contact />}></Route>
+				<Route path="/projects" element={<Projects />}></Route>
+				<Route path="*" element={<Error404 />}></Route>
+			</Routes>
+		</div>
 	)
 }
 
