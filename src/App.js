e4f37f269ebde9eb3eaff4a19e8320f453d94d8a
Kenny <kenji.maddela01@gmail.com>
2022-11-27 18:19:06 -0500

    Create template for the Footer component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 
+// Routes
 import Contact from './layout/Contact'
 import Home from './layout/Home'
 import Projects from './layout/Projects'
 import Error404 from './layout/Error404'
+
+// Essentials
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
 	return (
 		<div className="h-screen bg-red-500 text-5xl flex flex-col justify-between">
-			<Header></Header>
+			<Header />
 			<Routes>
 				Hello World!
 				<Route path="/" element={<Home />}></Route>
@@ -18,6 +22,7 @@ function App() {
 				<Route path="/projects" element={<Projects />}></Route>
 				<Route path="*" element={<Error404 />}></Route>
 			</Routes>
+			<Footer />
 		</div>
 	)
 }
